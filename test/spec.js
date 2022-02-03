5bde7c00cf0a65d582b2ef0af8bbc77d58f7f339
Kenneth Rosas <kencrosas@gmail.com>
2022-02-02 20:30:34 -0500

    First commit

---
+++ test/spec.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("This confirms that tests is being run", () => {
+  it("If this passes, comparisons is working", () => {
+    expect(true).to.equals(true);
+  });
+});
